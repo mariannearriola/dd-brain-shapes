92e00c4719d76be14483946db3c0d40c9832c0b6
mariannearriola <marianne.arriola@gmail.com>
2023-03-08 14:40:18 -0800

    Added rough outline of diffusion model

---
+++ utils.py
@@ -0,0 +1,14 @@
+import torch.nn.functional as F
+import torch
+
+def linear_beta_schedule(timesteps, start=0.0001, end=0.02):
+    return torch.linspace(start, end, timesteps)
+
+def get_index_from_list(vals, t, x_shape):
+    """ 
+    Returns a specific index t of a passed list of values vals
+    while considering the batch dimension.
+    """
+    batch_size = t.shape[0]
+    out = vals.gather(-1, t.cpu())
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
