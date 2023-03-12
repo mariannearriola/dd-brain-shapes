8dffe9f6ff7ce1973839658ccfeed1fe179b68d5
mariannearriola <marianne.arriola@gmail.com>
2023-03-11 19:02:07 -0800

    Added shape project files

--- shape_project.py
+++ shape_project.py
@@ -1,42 +1,76 @@
-import torch
-import torch.nn.functional as F
+import geomstats
+import math
+import geomstats.backend as gs
+from geomstats.geometry.spd_matrices import *
+from geomstats.geometry.lie_group import *
+from geomstats.datasets.utils import load_connectomes
+from geomstats.visualization.spd_matrices import Ellipses
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.linalg as npla
+from geomstats.learning.geodesic_regression import GeodesicRegression
+from geomstats.geometry.pre_shape import PreShapeSpace, KendallShapeMetric
+from geomstats.learning.frechet_mean import FrechetMean
+from geomstats.geometry.special_euclidean import SpecialEuclidean
+from geomstats.geometry.lie_group import LieGroup
+from geomstats.geometry.general_linear import GeneralLinear
+from shape_project import *
 
-def linear_beta_schedule(timesteps, start=0.0001, end=0.02):
-    return torch.linspace(start, end, timesteps)
+def unsqueeze(mat):
+    return gs.reshape(mat,(1,mat.shape[0],mat.shape[1]))
 
-def get_index_from_list(vals, t, x_shape):
-    """ 
-    Returns a specific index t of a passed list of values vals
-    while considering the batch dimension.
+def plot_mats(data,noisy_mats,ncol):
+    T = noisy_mats.shape[0]
+    figs,axs=plt.subplots(int(math.ceil((T+1)/(1*ncol))), ncol, figsize=(20,9))
+    axs[0][0].imshow(data)
+    axs[0][0].title.set_text("Input")
+    for im_id,noisy_mat in enumerate(noisy_mats):
+        axs[int((im_id+1)/ncol)][(im_id+1)%ncol].imshow(noisy_mat)
+        axs[int((im_id+1)/ncol)][(im_id+1)%ncol].title.set_text(f'T={im_id+1}')
+    return figs
+
+def denoise_mats(x_0,noisy_samples):
+    for ind,noisy_mat in enumerate(noisy_samples):
+        if ind == 0:
+            denoising_mats=unsqueeze(npla.solve(x_0,noisy_mat))
+        else:
+            denoising_mats=gs.concatenate((denoising_mats,unsqueeze(npla.solve(noisy_samples[ind-1],noisy_mat))),axis=0)
+    return denoising_mats
+
+def plot_results(test,noisy,pred):
+    figs,axs=plt.subplots(1, 3, figsize=(9,9))
+    axs[0].imshow(test)
+    axs[0].title.set_text("Test matrix")
+    axs[1].imshow(noisy)
+    axs[1].title.set_text("Noisy matrix")
+    axs[2].imshow(pred)
+    axs[2].title.set_text("Denoised matrix")
+    return figs
+
+def compute_sqr_dist(a, b, metric):
+    """Compute the Bures-Wasserstein squared distance.
+        
+    Compute the Riemannian squared distance between all 
+    combinations of healthy SPD matrices and 
+    schizophrenic SPD matrices.
+
+    Parameters
+    ----------
+    healthy_spd : array-like, shape=[..., n, n]
+        Point.
+    schiz_spd : array-like, shape=[..., n, n]
+        Point.
+    n : int
+        Size of matrix.
+
+    Returns
+    -------
+    sqrd_dist : array-like, shape=[...]
+        Riemannian squared distance of all SPD combinations.
     """
-    batch_size = t.shape[0]
-    out = vals.gather(-1, t.cpu())
-    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
-
-def forward_diffusion_sample(x_0, t, device="cpu"):
-    """ 
-    Takes an image and a timestep as input and 
-    returns the noisy version of it
-    """
-    noise = torch.randn_like(x_0)
-    sqrt_alphas_cumprod_t = get_index_from_list(sqrt_alphas_cumprod, t, x_0.shape)
-    sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
-        sqrt_one_minus_alphas_cumprod, t, x_0.shape
-    )
-    # mean + variance
-    return sqrt_alphas_cumprod_t.to(device) * x_0.to(device) \
-    + sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device), noise.to(device)
-
-
-# Define beta schedule
-T = 300
-betas = linear_beta_schedule(timesteps=T)
-
-# Pre-calculate different terms for closed form
-alphas = 1. - betas
-alphas_cumprod = torch.cumprod(alphas, axis=0)
-alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
-sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
-sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
-sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
-posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+    sqrd_dist = []
+    for i in range(len(a[:])):
+        for j in range(len(b[:])):
+            sqrd_dist.append(metric.squared_dist(a[i], b[j]))
+    return sqrd_dist
